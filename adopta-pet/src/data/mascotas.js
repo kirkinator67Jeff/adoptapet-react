afa8b3ea8a123c7ac2cede5746028e5411d07357
Laboratorio 320 <nicolas.perez121@inacapmail.cl>
2026-06-25 09:00:16 -0400

    feat: proyecto base con datos

---
+++ adopta-pet/src/data/mascotas.js
@@ -0,0 +1,12 @@
+export const mascotas = [
+    {
+        id: 1,
+        nombre: "Pelusa",
+        raza: "Mestizo",
+        edad: 2,
+        especie: "Perro",
+        descripcion: "Cachorro juguetón y cariñoso, ideal para familias con niños.",
+        caracteristicas: ["vacunado", "desparasitado", "sociable"],
+        adopcionUrgente: false
+    }
+]
